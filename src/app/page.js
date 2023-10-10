c95ddb9d487abb519dd788c137d96e5306f6dcc1
Aparajita <57309318+appy30@users.noreply.github.com>
2023-10-11 01:57:24 +0530

    Importing And Exporting Components
    this example is broken down into two component files now:
    
        Gallery.js:
            Defines the Profile component which is only used within the same file and is not exported.
            Exports the Gallery component as a default export.
        App.js:
            Imports Gallery as a default import from Gallery.js.
            Exports the root App component as a default export.

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,7 @@
-function Profile() {
-      return (
-        <img
-          src="https://i.imgur.com/MK3eW3As.jpg"
-          alt="Katherine Johnson"
-        />
-      );
-    }
-
+import Gallery from "./gallery";
 export default function Home() {
   return (
-    
-    
-        <section>
-          <h1>Amazing scientists</h1>
-          <Profile />
-          <Profile />
-          <Profile />
-        </section>
+        <Gallery />
       );
     }
     
