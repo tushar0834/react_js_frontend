947fc0db4a2fdb732fe650956519766fd3589bd1
Aparajita <57309318+appy30@users.noreply.github.com>
2023-10-22 02:31:01 +0530

    Write an expression inside JSX curly braces
    You can write it as src={baseUrl + person.imageId + person.imageSize + '.jpg'}.
    
        { opens the JavaScript expression
        baseUrl + person.imageId + person.imageSize + '.jpg' produces the correct URL string
        } closes the JavaScript expression

--- src/app/gallery.js
+++ src/app/gallery.js
@@ -9,7 +9,7 @@ function Profile() {
   }
   
   export  function Gallery() {
-    const sname = 'Amazing scientists'
+    const sname = 'Amazing scient'
     return (
       <section>
         <h1>{sname}</h1>
@@ -19,4 +19,32 @@ function Profile() {
       </section>
     );
   }
+  const baseUrl = 'https://i.imgur.com/';
+  const person = {
+    name: 'Gregorio Y. Zara',
+    imageId: '7vQD0fP',
+    imageSize: 's',
+    theme: {
+      backgroundColor: 'black',
+      color: 'pink'
+    }
+  };
+  export function TodoList() {
+    return (
+      <div style={person.theme}>
+        <h1>{person.name}'s Todos</h1>
+        <img
+          className="avatar"
+          src={baseUrl + person.imageId + person.imageSize + '.jpg'}
+          alt={person.name}
+        />
+        <ul>
+          <li>Improve the videophone</li>
+          <li>Prepare aeronautics lectures</li>
+          <li>Work on the alcohol-fuelled engine</li>
+        </ul>
+      </div>
+    );
+  }
+  
   
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import { Gallery } from "./gallery";
 import { FragmentsDemo } from "./fragmentdemo";
+import { TodoList } from "./gallery";
 export default function Home() {
   return (
     <>
       <Gallery />
       <FragmentsDemo />
+      <TodoList />
     </>
 
   );
