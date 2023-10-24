64a823bd52f942fca6a1d241e34ddbde92fde740
tushar0834 <tushar3n4@gmail.com>
2023-10-25 01:21:47 +0530

    conditional rendering (if , &)
    Conditional Rendering
    Your components will often need to display different things depending on different conditions. In React, you can conditionally render JSX using JavaScript syntax like if statements, &&, and ? : operators.
    
    Another common shortcut you’ll encounter is the JavaScript logical AND (&&) operator. Inside React components, it often comes up when you want to render some JSX when the condition is true, or render nothing otherwise. With &&, you could conditionally render the checkmark only if isPacked is true

--- src/app/gallery.js
+++ src/app/gallery.js
@@ -1,50 +1,60 @@
 function Profile() {
-    const imgpath = 'https://i.imgur.com/QIrZWGIs.jpg'
-    return (
+  let isDifferentImage = false;
+  var imgpath = ''
+  if (isDifferentImage) {
+    imgpath = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'
+  } else {
+    imgpath = 'https://i.imgur.com/QIrZWGIs.jpg'
+  }
+  return (
+    <>
       <img
         src={imgpath}
         alt="Alan L. Hart"
       />
-    );
+      <>
+        {isDifferentImage && '✔'}
+      </>
+    </>
+  );
+}
+
+export function Gallery() {
+  const sname = 'Amazing scient'
+  return (
+    <section>
+      <h1>{sname}</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+const baseUrl = 'https://i.imgur.com/';
+const person = {
+  name: 'Gregorio Y. Zara',
+  imageId: '7vQD0fP',
+  imageSize: 's',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
   }
-  
-  export  function Gallery() {
-    const sname = 'Amazing scient'
-    return (
-      <section>
-        <h1>{sname}</h1>
-        <Profile />
-        <Profile />
-        <Profile />
-      </section>
-    );
-  }
-  const baseUrl = 'https://i.imgur.com/';
-  const person = {
-    name: 'Gregorio Y. Zara',
-    imageId: '7vQD0fP',
-    imageSize: 's',
-    theme: {
-      backgroundColor: 'black',
-      color: 'pink'
-    }
-  };
-  export function TodoList() {
-    return (
-      <div style={person.theme}>
-        <h1>{person.name}'s Todos</h1>
-        <img
-          className="avatar"
-          src={baseUrl + person.imageId + person.imageSize + '.jpg'}
-          alt={person.name}
-        />
-        <ul>
-          <li>Improve the videophone</li>
-          <li>Prepare aeronautics lectures</li>
-          <li>Work on the alcohol-fuelled engine</li>
-        </ul>
-      </div>
-    );
-  }
-  
-  
+};
+export function TodoList() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src={baseUrl + person.imageId + person.imageSize + '.jpg'}
+        alt={person.name}
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
+  );
+}
+
